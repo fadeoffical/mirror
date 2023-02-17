ea447c5f06c39ffcdb70a812d8d356b1f0d95a14
fade <truefadeoffical@gmail.com>
2023-02-18 00:45:12 +0100

    Add javadocs to package info of fade.mirror

--- api/src/main/java/fade/mirror/package-info.java
+++ api/src/main/java/fade/mirror/package-info.java
@@ -1,1 +1,12 @@
+/**
+ * This package contains the classes and interfaces that compose the mirror API. The mirror API is a declarative API
+ * that allows the user to use java reflections in a more intuitive way. The API is composed by the following classes:
+ * <ul>
+ *     <li>{@link fade.mirror.Mirror}: the main class of the API. It is used to create a mirrors of reflection objects.</li>
+ *     <li>{@link fade.mirror.MClass}: the class that represents a mirror of a class.</li>
+ *     <li>{@link fade.mirror.MField}: the class that represents a mirror of a field.</li>
+ *     <li>{@link fade.mirror.MMethod}: the class that represents a mirror of a method.</li>
+ *     <li>{@link fade.mirror.MParameter}: the class that represents a mirror of a parameter.</li>
+ * </ul>
+ */
 package fade.mirror;
