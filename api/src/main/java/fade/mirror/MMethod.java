ce56dab315145da93dfbb027b2e0322d75e9bdf7
fade <truefadeoffical@gmail.com>
2023-02-17 22:33:17 +0100

    Add MMethod docs

--- api/src/main/java/fade/mirror/MMethod.java
+++ api/src/main/java/fade/mirror/MMethod.java
@@ -1,5 +1,26 @@
 package fade.mirror;
 
-public non-sealed interface MMethod<T> extends Invokable<T>, Accessible<MMethod<T>>, Copyable<MMethod<T>>, Parameterized, Annotated, Named, Declared, InstanceBindable {
+import fade.mirror.internal.impl.MMethodImpl;
+
+/**
+ * Represents a method of a {@link MClass}. A method has a name, a return type, a list of parameters and a list of exceptions.
+ * It may also have annotations.
+ *
+ * @param <T> the type of the method.
+ * @author fade <truefadeoffical@gmail.com>
+ */
+public sealed interface MMethod<T>
+        extends Invokable<T>,
+                Accessible<MMethod<T>>,
+                Copyable<MMethod<T>>,
+                Parameterized,
+                Annotated,
+                Named,
+                Declared,
+                InstanceBindable
+        permits MMethodImpl {
+
+    // todo: return type
+    // todo: exceptions
 
 }
