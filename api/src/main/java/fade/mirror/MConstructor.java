88367a9cbfb07a04cfc9390b24fa852b349791a7
fade <truefadeoffical@gmail.com>
2023-02-17 22:58:26 +0100

    Add mconstructor docs

--- api/src/main/java/fade/mirror/MConstructor.java
+++ api/src/main/java/fade/mirror/MConstructor.java
@@ -1,11 +1,25 @@
 package fade.mirror;
 
+import fade.mirror.internal.impl.MConstructorImpl;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Constructor;
 
-public non-sealed interface MConstructor<T> extends Invokable<T>, Accessible<MConstructor<T>>, Parameterized, Annotated, Declared {
+/**
+ * Represents a constructor of a {@link MClass}. A constructor has parameters and may also have annotations.
+ *
+ * @param <T> the type of the class that declares this constructor
+ * @author fade <truefadeoffical@gmail.com>
+ */
+public sealed interface MConstructor<T>
+        extends Invokable<T>, Accessible<MConstructor<T>>, Parameterized, Annotated, Declared
+        permits MConstructorImpl {
 
+    /**
+     * Returns the raw constructor. This is the constructor that is represented by this object.
+     *
+     * @return the raw constructor.
+     */
     @NotNull Constructor<T> getRawConstructor();
 
 }
