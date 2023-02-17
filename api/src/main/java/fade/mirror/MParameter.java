30aa60f8f723dc8de62b0ecd2c0dfd3da94872ba
fade <truefadeoffical@gmail.com>
2023-02-17 22:27:39 +0100

    Add MParameter documentation

--- api/src/main/java/fade/mirror/MParameter.java
+++ api/src/main/java/fade/mirror/MParameter.java
@@ -2,7 +2,18 @@ package fade.mirror;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a parameter of a {@link MMethod} or {@link MConstructor}. A parameter has a name and a type. It may also
+ * have annotations.
+ *
+ * @param <T> the type of the parameter
+ */
 public interface MParameter<T> extends Annotated, Named {
 
+    /**
+     * Returns the type of this parameter represented by a java class.
+     *
+     * @return the type.
+     */
     @NotNull Class<T> getType();
 }
