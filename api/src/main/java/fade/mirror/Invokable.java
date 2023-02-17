adc2ca8588267d034dbf13658b0f144f83e18d63
fade <truefadeoffical@gmail.com>
2023-02-18 00:21:36 +0100

    Add javadocs to invokable

--- api/src/main/java/fade/mirror/Invokable.java
+++ api/src/main/java/fade/mirror/Invokable.java
@@ -2,9 +2,27 @@ package fade.mirror;
 
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a method or constructor that can be invoked.
+ *
+ * @param <T> the return type of the method or constructor
+ * @author fade <truefadeoffical@gmail.com>
+ */
 public interface Invokable<T> {
 
+    /**
+     * Invokes the method or constructor represented by this object.
+     *
+     * @param arguments the arguments to the method or constructor.
+     * @return the result of the method or constructor.
+     */
     @Nullable T invoke(@Nullable Object... arguments);
 
+    /**
+     * Tests whether the method or constructor represented by this object can be invoked with the given arguments.
+     *
+     * @param arguments the arguments to test.
+     * @return whether the method or constructor can be invoked with the given arguments.
+     */
     boolean invokableWith(@Nullable Object... arguments);
 }
