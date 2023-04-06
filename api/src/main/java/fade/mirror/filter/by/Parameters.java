924af14784667ce02a084fa5dcde3c1f77a245de
fade <truefadeoffical@gmail.com>
2023-04-07 00:45:39 +0200

    Fix formatting

--- api/src/main/java/fade/mirror/filter/by/Parameters.java
+++ api/src/main/java/fade/mirror/filter/by/Parameters.java
@@ -11,18 +11,15 @@ public interface Parameters<R> {
 
     @NotNull R withParameters(@NotNull List<Class<?>> parameterTypes, @NotNull RewriteOperation operation);
 
-    @NotNull
-    default R withParameters(@NotNull List<Class<?>> parameterTypes) {
+    default @NotNull R withParameters(@NotNull List<Class<?>> parameterTypes) {
         return this.withParameters(parameterTypes, RewriteOperation.Append);
     }
 
-    @NotNull
-    default R withParameter(@NotNull Class<?> parameterType, @NotNull RewriteOperation operation) {
+    default @NotNull R withParameter(@NotNull Class<?> parameterType, @NotNull RewriteOperation operation) {
         return this.withParameters(List.of(parameterType), operation);
     }
 
-    @NotNull
-    default R withParameter(@NotNull Class<?> parameterType) {
+    default @NotNull R withParameter(@NotNull Class<?> parameterType) {
         return this.withParameter(parameterType, RewriteOperation.Append);
     }
 }
